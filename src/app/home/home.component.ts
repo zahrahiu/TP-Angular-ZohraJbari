fe7e37a566d8df0046f427576b0ded089e52e927
zahra <zohrajbari2003@gmail.com>
2025-05-19 00:39:08 +0100

    Modifier page home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,46 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { CartService } from '../Cart/cart.service';
-import { Product } from '../models/product.model';
+// home.component.ts
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-  featuredProducts: Product[] = [];
-  testimonials = [
-    {name: 'Sarah', comment: 'Les parfums sont fabuleux, je recommande vivement!'},
-    {name: 'Youssef', comment: 'Livraison rapide et service impeccable.'},
-    {name: 'Nadia', comment: 'Qualité et senteurs exceptionnelles.'}
-  ];
-
-  constructor(private router: Router, private cartService: CartService) {}
-
-  ngOnInit(): void {
-    this.cartService.getFeaturedProducts().subscribe(data => {
-      this.featuredProducts = data;
-    });
-  }
-
-  goToCatalog() {
-    this.router.navigate(['/catalog']);
-  }
-
-  goToCategory(category: string) {
-    this.router.navigate(['/catalog'], { queryParams: { category } });
-  }
-
-  goToOffers() {
-    this.router.navigate(['/offers']);
-  }
-
-  goToSignin() {
-    this.router.navigate(['/signin']);
-  }
-
-  viewDetails(productId: number) {
-    this.router.navigate(['/product-details'], { queryParams: { id: productId } });
-  }
+export class HomeComponent {
+  // Vous pouvez ajouter des fonctions de navigation si besoin
 }
